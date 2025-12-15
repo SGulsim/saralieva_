ea49cb36c13580963070270d6d49d6d43d454f15
SGulsim <saralievagulsim@gmail.com>
2025-12-15 18:34:03 +0300

    kr04: только файлы из папки kr04

---
+++ kr04/semaphore/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+namespace semaphore;
+class Program
+{
+    static void Main()
+    {
+        var semaphore = new SemaphoreSlim(3);
+        var threads = new Thread[10];
+
+        for (int i = 0; i < threads.Length; i++)
+        {
+            int id = i;
+            threads[i] = new Thread(() =>
+            {
+                semaphore.Wait();
+                try
+                {
+                    Console.WriteLine($"начали поток айдишник {id}");
+                    Thread.Sleep(1000);
+                    Console.WriteLine($"завершили поток айдишинк {id}");
+                }
+                finally
+                {
+                    semaphore.Release();
+                }
+            });
+            threads[i].Start();
+        }
+
+        foreach (var thread in threads)
+            thread.Join();
+    }
+}
